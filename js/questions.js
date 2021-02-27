4a2a0b119d43ba52f6996c1afb07c612423e4ca7
Katz Bengoshi <pkatz@finkkatz.com>
2021-02-27 12:29:53 -0500

    initial push of alternative for HW 4

---
+++ js/questions.js
@@ -0,0 +1,40 @@
+let MCQS = [{
+    question: "The First Amendment of the US Constitution protects which of the following rights?",
+    choice1: "Free Speech",
+    choice2: "Right to bear arms",
+    choice3: "Right not to be tried for the same offense twice",
+    choice4: "Speedy Trial",
+    answer: 0
+},
+            {
+            question: "The Fourth Amendment of the US Constitution protects which of the following rights?",
+    choice1: "Free Speech",
+    choice2: "Speedy Trial",
+    choice3: "Right against warrantless searches",
+    choice4: "Right to Due Process",
+    answer: 2
+},
+            {
+            question: "The Fifth Amendment of the US Constitution protects which of the following rights?",
+    choice1: "Speedy Trial",
+    choice2: "Freedom of Religion",
+    choice3: "Right to remain silent",
+    choice4: "Right to due process",
+    answer: 2
+},
+            {
+            question: "Which US Constitutional Amendment effectively ended slavery?",
+    choice1: "Fifth",
+    choice2: "Sixth",
+    choice3: "Twelfth",
+    choice4: "Thirteenth",
+    answer: 3
+},
+            {
+            question: "If a person chooses not to testify at his own criminal trial, what US Constitutional right is he relying upon?",
+    choice1: "13th",
+    choice2: "5th",
+    choice3: "12th",
+    choice4: "1st",
+    answer: 1
+}];
---
+++ js/script.js
@@ -0,0 +1,166 @@
+//Start Section
+let start = document.querySelector("#start");
+
+//guide Section
+let guide = document.querySelector("#guide");
+let exit = document.querySelector("#exit");
+let continueBtn = document.querySelector("#continue");
+
+//Quiz Section
+let quiz = document.querySelector("#quiz");
+let time = document.querySelector("#time");
+
+//question Section
+let questionNo = document.querySelector("#questionNo");
+let questionText = document.querySelector("#questionText");
+
+//Multiple Choice Questions
+let option1 = document.querySelector("#option1");
+let option2 = document.querySelector("#option2");
+let option3 = document.querySelector("#option3");
+let option4 = document.querySelector("#option4");
+
+//correct and next Button
+let total_correct = document.querySelector("#total_correct");
+let next_question = document.querySelector("#next_question");
+
+//Result Section
+let result = document.querySelector("#result");
+let points = document.querySelector("#points");
+let quit = document.querySelector("#quit");
+let startAgain = document.querySelector("#startAgain");
+
+//Get All 'H4' From Quiz Section (MCQS)
+let choice_que = document.querySelectorAll(".choice_que");
+
+
+let index = 0;
+let timer = 0;
+let interval = 0;
+
+//total points
+let correct = 0;
+
+//stores Answer Value
+let UserAns = undefined;
+
+//action when 'Start' Button is Clicked
+start.addEventListener("click", () => {
+    start.style.display = "none";
+    guide.style.display = "block";
+});
+
+//action when 'Exit' Button is Clicked
+exit.addEventListener("click", () => {
+    start.style.display = "block";
+    guide.style.display = "none";
+});
+
+
+//Creates Timer For Quiz Timer Section
+
+let countDown = () => {
+    if (timer === 20) {
+        clearInterval(interval);
+        next_question.click();
+    } else {
+        timer++;
+        time.innerText = timer;
+    }
+}
+
+//setInterval(countDown,1000);
+
+let loadData = () => {
+    questionNo.innerText = index + 1 + ". ";
+    questionText.innerText = MCQS[index].question;
+    option1.innerText = MCQS[index].choice1;
+    option2.innerText = MCQS[index].choice2;
+    option3.innerText = MCQS[index].choice3;
+    option4.innerText = MCQS[index].choice4;
+
+    //    timer start
+    timer = 0;
+}
+
+loadData();
+
+//action when 'Continue' Button is clicked
+continueBtn.addEventListener("click", () => {
+    quiz.style.display = "block";
+    guide.style.display = "none";
+
+    interval = setInterval(countDown, 1000);
+    loadData();
+
+    //    remove All Active Classes When Continue Button is clicked
+
+    choice_que.forEach(removeActive => {
+        removeActive.classList.remove("active");
+    })
+
+    total_correct.innerHTML = `${correct = 0} Out Of ${MCQS.length} Questions`;
+});
+
+choice_que.forEach((choices, choiceNo) => {
+    choices.addEventListener("click", () => {
+        choices.classList.add("active");
+        //check answer
+        if (choiceNo === MCQS[index].answer) {
+            correct++;
+        } else {
+            correct += 0;
+        }
+        //stop Counter
+        clearInterval(interval);
+
+        //disables All Options When User Selects An Option
+        for (i = 0; i <= 3; i++) {
+            choice_que[i].classList.add("disabled");
+        }
+    })
+});
+
+////action when 'Next' Button is clicked
+next_question.addEventListener("click", () => {
+    //    if index is less then MCQS.length
+    if (index !== MCQS.length - 1) {
+        index++;
+        choice_que.forEach(removeActive => {
+            removeActive.classList.remove("active");
+        })
+
+        //question
+        loadData();
+
+        //result
+        total_correct.style.display = "block";
+        total_correct.innerHTML = `${correct} Out Of ${MCQS.length} Questions`;
+        clearInterval(interval);
+        interval = setInterval(countDown, 1000);
+    } else {
+        index = 0;
+
+
+        //action when Quiz Questions are Complete; Displays Result Section
+        clearInterval(interval);
+        quiz.style.display = "none";
+        points.innerHTML = `You Got ${correct} Out Of ${MCQS.length}`;
+        result.style.display = "block";
+    }
+    for (i = 0; i <= 3; i++) {
+        choice_que[i].classList.remove("disabled");
+    }
+})
+
+//action when 'Quit' Button is clicked
+quit.addEventListener("click", () => {
+    start.style.display = "block";
+    result.style.display = "none";
+});
+
+//Action to start again When 'Start Again' Button is clicked
+startAgain.addEventListener("click", () => {
+    guide.style.display = "block";
+    result.style.display = "none";
+});
